c8b312f5af1c93dabd6fdbcbf2d536a9aa135c93
Midou0-dar <mabanemohammedzouhir@gmail.com>
2025-02-09 12:51:01 +0100

    Add catch-all route for NotFound component

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
 import { Scores } from './pages/Scores';
+import NotFound from './pages/NotFound'; // Import NotFound component
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/scores" element={<Scores />} />
+          <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
         </Routes>
       </div>
     </BrowserRouter>
