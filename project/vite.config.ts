9f74d04b6beefb924b3c27d0f34125612f172f38
Midou0-dar <mabanemohammedzouhir@gmail.com>
2025-02-09 13:04:12 +0100

    Ensure correct rewrites for SPA routing and update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Set the base path for deployment
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Allows access from any IP address
